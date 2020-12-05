92e7b72cd616eb526470348b448197653d0078b5
Balasundaram Avudai Nayagam <balasundaram5396@gmail.com>
2020-12-04 19:38:55 -0500

    JS

--- script_1.js
+++ script_1.js
@@ -1,111 +1,147 @@
+//defining helper functions
+
+//defining deep copy method
+Array.prototype.copyDeep = function () {
+  var result = [];
+  for (var i = 0; i < this.length; i++) {
+    if (this[i].isArray) result[i] = this[i].copyDeep();
+    else result[i] = this[i];
+  }
+  return result;
+};
+
 Array.prototype.isArray = true;
+// defining pop method
+Array.prototype.remove = function (e) {
+  var x = this.indexOf(e);
+  if (x > -1) this.splice(x, 1);
+};
+
+//defining toString method for string conversion
 Array.prototype.toString = function () {
   return '[' + this.join(',') + ']';
 };
 
 if (!Array.prototype.includes) {
   Array.prototype.includes = function (e) {
-    for (var j = 0; j < this.length; j++) {
-      if (this[j] == e) return true;
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] == e) return true;
     }
     return false;
   };
 }
-Array.prototype.remove = function (e) {
-  var ix1 = this.indexOf(e);
-  if (ix1 > -1) this.splice(ix1, 1);
-};
-Array.prototype.intersect = function (es) {
-  for (var j = 0; j < this.length; j++) {
-    if (es.indexOf(this[j]) == -1) {
-      this.splice(j--, 1);
+
+// defining intersect method to find a element is present in the array
+Array.prototype.intersect = function (e) {
+  for (var i = 0; i < this.length; i++) {
+    if (e.indexOf(this[i]) == -1) {
+      this.splice(i--, 1);
     }
   }
 };
-Array.prototype.insert = function (e, ix1) {
-  return this.splice(ix1, 0, e);
+
+//defining push function
+Array.prototype.insert = function (e, x) {
+  return this.splice(x, 0, e);
 };
-Array.prototype.concatNoDuplicates = function (array1) {
-  var h1 = {};
-  var r1 = [];
-  for (var j = 0; j < this.length; j++) {
-    h1[this[j].toString()] = true;
-    r1.push(this[j]);
-  }
-  for (var j = 0; j < array1.length; j++) {
-    var s = array1[j].toString();
-    if (!h1[s]) {
-      h1[s] = true;
-      r1.push(array1[j]);
-    }
-  }
-  return r1;
-};
-Array.prototype.removeDuplicates = function () {
-  var h1 = {};
-  var r1 = [];
-  for (var j = 0; j < this.length; j++) {
-    var s = this[j].toString();
-    if (!h1[s]) {
-      h1[s] = true;
-      r1.push(this[j]);
-    }
-  }
-  return r1;
-};
-Array.getArrayOfZeroes = function (length) {
-  for (var j = 0, a = new Array(length); j < length; ) a[j++] = 0;
+
+//defining getter method
+Array.getArrayOfNumbers = function (length) {
+  for (var i = 0, a = new Array(length); i < length; i++) a[i] = i;
   return a;
 };
 
-Array.getArrayOfNumbers = function (length) {
-  for (var j = 0, a = new Array(length); j < length; j++) a[j] = j;
-  return a;
-};
-Array.prototype.copy = function () {
-  var rs1 = [];
-  for (var j = 0; j < this.length; j++) rs1[j] = this[j];
-  return rs1;
-};
-Array.prototype.copyDeep = function () {
-  var rs1 = [];
-  for (var j = 0; j < this.length; j++) {
-    if (this[j].isArray) rs1[j] = this[j].copyDeep();
-    else rs1[j] = this[j];
+Array.prototype.concatNoDuplicates = function (array1) {
+  var h1 = {};
+  var r1 = [];
+  for (var i = 0; i < this.length; i++) {
+    h1[this[i].toString()] = true;
+    r1.push(this[i]);
   }
-  return rs1;
+  for (var i = 0; i < array1.length; i++) {
+    var s = array1[i].toString();
+    if (!h1[s]) {
+      h1[s] = true;
+      r1.push(array1[i]);
+    }
+  }
+  return r1;
 };
-Array.prototype.equals = function (arr1) {
-  if (this.length != arr1.length) return false;
-  for (var j = 0; j < this.length; j++) {
-    if (this[j].isArray) {
-      if (!arr1[j].isArray) return false;
-      if (!this[j].equals(arr1[j])) return false;
-    } else if (this[j] != arr1[j]) return false;
+
+Array.prototype.equals = function (list) {
+  if (this.length != list.length) return false;
+  for (var i = 0; i < this.length; i++) {
+    if (this[i].isArray) {
+      if (!list[i].isArray) {
+        return false;
+      }
+      if (!this[i].equals(list[i])) {
+        return false;
+      }
+    } else if (this[i] != list[i]) {
+      return false;
+    }
   }
   return true;
 };
+// method to remove duplicates
+Array.prototype.removeDuplicates = function () {
+  var new1 = {};
+  var old = [];
+  for (var i = 0; i < this.length; i++) {
+    var temp = this[i].toString();
+    if (!new1[temp]) {
+      new1[temp] = true;
+      old.push(this[i]);
+    }
+  }
+  return old;
+};
+
+Array.getArrayOfZeroes = function (length) {
+  for (var i = 0, a = new Array(length); i < length; ) a[i++] = 0;
+  return a;
+};
+// method to copy the values to a new array
+Array.prototype.copy = function () {
+  var result = [];
+  for (var i = 0; i < this.length; i++) result[i] = this[i];
+  return result;
+};
+
 function Formula() {}
 Formula.prototype.toString = function () {
   return this.string;
 };
-Formula.prototype.equals = function (fl1) {
-  return this.string == fl1.string;
+Formula.prototype.equals = function (formula) {
+  return this.string == formula.string;
 };
 Formula.prototype.negate = function () {
   return new NegatedFormula(this);
 };
 Formula.prototype.unify = function (formula) {
-  if (this.type != 'literal') return false;
-  if (this.sub && !formula.sub) return false;
-  if (this.sub) return this.sub.unify(formula.sub);
-  if (this.predicate != formula.predicate) return false;
-  if (this.terms.length != formula.terms.length) return false;
-  var merge = [];
-  var items1 = this.terms.copyDeep();
-  var items2 = formula.terms.copyDeep();
+  var res = [];
   var i1, i2;
-  while (((i1 = items1.shift()), (i2 = items2.shift()))) {
+  if (this.predicate != formula.predicate) {
+    return false;
+  }
+  if (this.type != 'literal') {
+    return false;
+  }
+  if (this.terms.length != formula.terms.length) {
+    return false;
+  }
+  if (this.sub && !formula.sub) {
+    return false;
+  }
+  if (this.sub) {
+    return this.sub.unify(formula.sub);
+  }
+
+  var val1 = this.terms.copyDeep();
+  var val2 = formula.terms.copyDeep();
+
+  while (((i1 = val1.shift()), (i2 = val2.shift()))) {
     log('unify terms? ' + i1 + ' <=> ' + i2);
     if (i1 == i2) {
       continue;
@@ -113,8 +149,8 @@ Formula.prototype.unify = function (formula) {
     if (i1.isArray && i2.isArray) {
       if (i1[0] != i2[0]) return false;
       for (var i = 1; i < i1.length; i++) {
-        items1.push(i1[i]);
-        items2.push(i2[i]);
+        val1.push(i1[i]);
+        val2.push(i2[i]);
       }
       continue;
     }
@@ -141,17 +177,17 @@ Formula.prototype.unify = function (formula) {
     }
     log('yes: unify');
     var terms,
-      termss = [merge, items1, items2];
+      termss = [res, val1, val2];
     while ((terms = termss.shift())) {
       for (var i = 0; i < terms.length; i++) {
         if (terms[i].isArray) termss.push(terms[i]);
         else if (terms[i] == i1) terms[i] = i2;
       }
     }
-    merge.push(i1);
-    merge.push(i2);
+    res.push(i1);
+    res.push(i2);
   }
-  return merge;
+  return res;
 };
 Formula.prototype.normalize = function () {
   var op = this.operator || this.quantifier;
@@ -322,8 +358,8 @@ AtomicFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
 };
 AtomicFormula.substituteInTerms = function (terms, origTerm, newTerm, shallow) {
   var newitems = [];
-  for (var j = 0; j < terms.length; j++) {
-    var term = terms[j];
+  for (var i = 0; i < terms.length; i++) {
+    var term = terms[i];
     if (term == origTerm) newitems.push(newTerm);
     else if (term.isArray && !shallow) {
       newitems.push(AtomicFormula.substituteInTerms(term, origTerm, newTerm));
