525d6591d16f2ed64108c644423d0c9b83951c49
Balasundaram Avudai Nayagam <balasundaram5396@gmail.com>
2020-12-04 20:37:20 -0500

    JS

--- script_1.js
+++ script_1.js
@@ -51,21 +51,21 @@ Array.getArrayOfNumbers = function (length) {
   return a;
 };
 
-Array.prototype.concatNoDuplicates = function (array1) {
-  var h1 = {};
-  var r1 = [];
+Array.prototype.concatNoDuplicates = function (arr) {
+  var a = {};
+  var b = [];
   for (var i = 0; i < this.length; i++) {
-    h1[this[i].toString()] = true;
-    r1.push(this[i]);
+    a[this[i].toString()] = true;
+    b.push(this[i]);
   }
-  for (var i = 0; i < array1.length; i++) {
-    var s = array1[i].toString();
-    if (!h1[s]) {
-      h1[s] = true;
-      r1.push(array1[i]);
+  for (var i = 0; i < arr.length; i++) {
+    var s = arr[i].toString();
+    if (!a[s]) {
+      a[s] = true;
+      b.push(arr[i]);
     }
   }
-  return r1;
+  return b;
 };
 
 Array.prototype.equals = function (list) {
@@ -110,6 +110,38 @@ Array.prototype.copy = function () {
 };
 
 function Formula() {}
+Formula.prototype.alpha = function (val) {
+  if (this.operator == '∧') {
+    return val == 1 ? this.sub1 : this.sub2;
+  }
+  if (this.sub.operator == '∨') {
+    return val == 1 ? this.sub.sub1.negate() : this.sub.sub2.negate();
+  }
+  if (this.sub.operator == '→') {
+    return val == 1 ? this.sub.sub1 : this.sub.sub2.negate();
+  }
+};
+Formula.prototype.beta = function (val) {
+  if (this.operator == '→') {
+    return val == 1 ? this.sub1.negate() : this.sub2;
+  }
+  if (this.operator == '∨') {
+    return val == 1 ? this.sub1 : this.sub2;
+  }
+  if (this.operator == '↔') {
+    return val == 1
+      ? new BinaryFormula('∧', this.sub1, this.sub2)
+      : new BinaryFormula('∧', this.sub1.negate(), this.sub2.negate());
+  }
+  if (this.sub.operator == '∧') {
+    return val == 1 ? this.sub.sub1.negate() : this.sub.sub2.negate();
+  }
+  if (this.sub.operator == '↔') {
+    return val == 1
+      ? new BinaryFormula('∧', this.sub.sub1, this.sub.sub2.negate())
+      : new BinaryFormula('∧', this.sub.sub1.negate(), this.sub.sub2);
+  }
+};
 Formula.prototype.toString = function () {
   return this.string;
 };
@@ -119,6 +151,96 @@ Formula.prototype.equals = function (formula) {
 Formula.prototype.negate = function () {
   return new NegatedFormula(this);
 };
+Formula.prototype.normalize = function () {
+  var sym = this.operator || this.quantifier;
+  if (!sym) return this;
+  switch (sym) {
+    case '∧':
+    case '∨': {
+      var sub1 = this.sub1.normalize();
+      var sub2 = this.sub2.normalize();
+      return new BinaryFormula(sym, sub1, sub2);
+    }
+    case '↔': {
+      var sub1 = new BinaryFormula('∧', this.sub1, this.sub2).normalize();
+      var sub2 = new BinaryFormula(
+        '∧',
+        this.sub1.negate(),
+        this.sub2.negate()
+      ).normalize();
+      return new BinaryFormula('∨', sub1, sub2);
+    }
+    case '→': {
+      var sub1 = this.sub1.negate().normalize();
+      var sub2 = this.sub2.normalize();
+      return new BinaryFormula('∨', sub1, sub2);
+    }
+    case '∀':
+    case '∃': {
+      return new QuantifiedFormula(
+        sym,
+        this.variable,
+        this.matrix.normalize(),
+        this.newVal
+      );
+    }
+    case '□':
+    case '◇': {
+      return new ModalFormula(sym, this.sub.normalize());
+    }
+    case '¬': {
+      var sym2 = this.sub.operator || this.sub.quantifier;
+      if (!sym2) return this;
+      switch (sym2) {
+        case '∧':
+        case '∨': {
+          var sub1 = this.sub.sub1.negate().normalize();
+          var sub2 = this.sub.sub2.negate().normalize();
+          var newOp = sym2 == '∧' ? '∨' : '∧';
+          return new BinaryFormula(newOp, sub1, sub2);
+        }
+        case '↔': {
+          var sub1 = new BinaryFormula(
+            '∧',
+            this.sub.sub1,
+            this.sub.sub2.negate()
+          ).normalize();
+          var sub2 = new BinaryFormula(
+            '∧',
+            this.sub.sub1.negate(),
+            this.sub.sub2
+          ).normalize();
+          return new BinaryFormula('∨', sub1, sub2);
+        }
+        case '→': {
+          var sub1 = this.sub.sub1.normalize();
+          var sub2 = this.sub.sub2.negate().normalize();
+          return new BinaryFormula('∧', sub1, sub2);
+        }
+
+        case '∀':
+        case '∃': {
+          var sub = this.sub.matrix.negate().normalize();
+          return new QuantifiedFormula(
+            sym2 == '∀' ? '∃' : '∀',
+            this.sub.variable,
+            sub,
+            this.sub.newVal
+          );
+        }
+        case '□':
+        case '◇': {
+          var sub = this.sub.sub.negate().normalize();
+          return new ModalFormula(sym2 == '□' ? '◇' : '□', sub);
+        }
+        case '¬': {
+          return this.sub.sub.normalize();
+        }
+      }
+    }
+  }
+};
+
 Formula.prototype.unify = function (formula) {
   var res = [];
   var i1, i2;
@@ -154,33 +276,31 @@ Formula.prototype.unify = function (formula) {
       }
       continue;
     }
-    var i10 = i1[0] == 'ξ' || i1[0] == 'ζ';
-    var i20 = i2[0] == 'ξ' || i2[0] == 'ζ';
-    if (!i10 && !i20) {
-      log('no, neither term variable');
+    var ival = i1[0] == 'ξ' || i1[0] == 'ζ';
+    var ival2 = i2[0] == 'ξ' || i2[0] == 'ζ';
+    if (!ival && !ival2) {
       return false;
     }
-    if (!i10) {
+    if (!ival) {
       var temp = i1;
       i1 = i2;
       i2 = temp;
     }
     if (i2.isArray) {
       var terms,
-        termss = [i2];
-      while ((terms = termss.shift())) {
+        term_val = [i2];
+      while ((terms = term_val.shift())) {
         for (var i = 0; i < terms.length; i++) {
-          if (terms[i].isArray) termss.push(terms[i]);
+          if (terms[i].isArray) term_val.push(terms[i]);
           else if (terms[i] == i1) return false;
         }
       }
     }
-    log('yes: unify');
     var terms,
-      termss = [res, val1, val2];
-    while ((terms = termss.shift())) {
+      term_val = [res, val1, val2];
+    while ((terms = term_val.shift())) {
       for (var i = 0; i < terms.length; i++) {
-        if (terms[i].isArray) termss.push(terms[i]);
+        if (terms[i].isArray) term_val.push(terms[i]);
         else if (terms[i] == i1) terms[i] = i2;
       }
     }
@@ -189,251 +309,56 @@ Formula.prototype.unify = function (formula) {
   }
   return res;
 };
-Formula.prototype.normalize = function () {
-  var op = this.operator || this.quantifier;
-  if (!op) return this;
-  switch (op) {
-    case '∧':
-    case '∨': {
-      var sub1 = this.sub1.normalize();
-      var sub2 = this.sub2.normalize();
-      return new BinaryFormula(op, sub1, sub2);
-    }
-    case '→': {
-      var sub1 = this.sub1.negate().normalize();
-      var sub2 = this.sub2.normalize();
-      return new BinaryFormula('∨', sub1, sub2);
-    }
-    case '↔': {
-      var sub1 = new BinaryFormula('∧', this.sub1, this.sub2).normalize();
-      var sub2 = new BinaryFormula(
-        '∧',
-        this.sub1.negate(),
-        this.sub2.negate()
-      ).normalize();
-      return new BinaryFormula('∨', sub1, sub2);
-    }
-    case '∀':
-    case '∃': {
-      return new QuantifiedFormula(
-        op,
-        this.variable,
-        this.matrix.normalize(),
-        this.overWorlds
-      );
-    }
-    case '□':
-    case '◇': {
-      return new ModalFormula(op, this.sub.normalize());
-    }
-    case '¬': {
-      var op2 = this.sub.operator || this.sub.quantifier;
-      if (!op2) return this;
-      switch (op2) {
-        case '∧':
-        case '∨': {
-          var sub1 = this.sub.sub1.negate().normalize();
-          var sub2 = this.sub.sub2.negate().normalize();
-          var newOp = op2 == '∧' ? '∨' : '∧';
-          return new BinaryFormula(newOp, sub1, sub2);
-        }
-        case '→': {
-          var sub1 = this.sub.sub1.normalize();
-          var sub2 = this.sub.sub2.negate().normalize();
-          return new BinaryFormula('∧', sub1, sub2);
-        }
-        case '↔': {
-          var sub1 = new BinaryFormula(
-            '∧',
-            this.sub.sub1,
-            this.sub.sub2.negate()
-          ).normalize();
-          var sub2 = new BinaryFormula(
-            '∧',
-            this.sub.sub1.negate(),
-            this.sub.sub2
-          ).normalize();
-          return new BinaryFormula('∨', sub1, sub2);
-        }
-        case '∀':
-        case '∃': {
-          var sub = this.sub.matrix.negate().normalize();
-          return new QuantifiedFormula(
-            op2 == '∀' ? '∃' : '∀',
-            this.sub.variable,
-            sub,
-            this.sub.overWorlds
-          );
-        }
-        case '□':
-        case '◇': {
-          var sub = this.sub.sub.negate().normalize();
-          return new ModalFormula(op2 == '□' ? '◇' : '□', sub);
-        }
-        case '¬': {
-          return this.sub.sub.normalize();
-        }
-      }
-    }
-  }
-};
+
 Formula.prototype.removeQuantifiers = function () {
   if (this.matrix) return this.matrix.removeQuantifiers();
   if (this.sub1) {
-    var ns11 = this.sub1.quantifier
+    var num1 = this.sub1.quantifier
       ? this.sub1.matrix.removeQuantifiers()
       : this.sub1.removeQuantifiers();
-    var ns12 = this.sub2.quantifier
+    var num2 = this.sub2.quantifier
       ? this.sub2.matrix.removeQuantifiers()
       : this.sub2.removeQuantifiers();
-    if (this.sub1 == ns11 && this.sub2 == ns12) return this;
-    var r1 = new BinaryFormula(this.operator, ns11, ns12);
-    return r1;
+    if (this.sub1 == num1 && this.sub2 == num2) return this;
+    var b = new BinaryFormula(this.operator, num1, num2);
+    return b;
   }
   return this;
 };
-Formula.prototype.alpha = function (n) {
-  if (this.operator == '∧') {
-    return n == 1 ? this.sub1 : this.sub2;
-  }
-  if (this.sub.operator == '∨') {
-    return n == 1 ? this.sub.sub1.negate() : this.sub.sub2.negate();
-  }
-  if (this.sub.operator == '→') {
-    return n == 1 ? this.sub.sub1 : this.sub.sub2.negate();
-  }
-};
-Formula.prototype.beta = function (n) {
-  if (this.operator == '∨') {
-    return n == 1 ? this.sub1 : this.sub2;
-  }
-  if (this.operator == '→') {
-    return n == 1 ? this.sub1.negate() : this.sub2;
-  }
-  if (this.operator == '↔') {
-    return n == 1
-      ? new BinaryFormula('∧', this.sub1, this.sub2)
-      : new BinaryFormula('∧', this.sub1.negate(), this.sub2.negate());
-  }
-  if (this.sub.operator == '∧') {
-    return n == 1 ? this.sub.sub1.negate() : this.sub.sub2.negate();
-  }
-  if (this.sub.operator == '↔') {
-    return n == 1
-      ? new BinaryFormula('∧', this.sub.sub1, this.sub.sub2.negate())
-      : new BinaryFormula('∧', this.sub.sub1.negate(), this.sub.sub2);
-  }
-};
-function AtomicFormula(predicate, terms) {
-  this.type = 'literal';
-  this.terms = terms;
-  this.predicate = predicate;
-  this.string = predicate + AtomicFormula.items2string(terms);
-}
-AtomicFormula.items2string = function (list) {
-  var r1 = '';
-  for (var i = 0; i < list.length; i++) {
-    if (list[i].isArray) {
-      var slist = list[i].copy();
-      var fs = slist.shift();
-      r1 += fs + '(' + AtomicFormula.items2string(slist) + ')';
-    } else r1 += list[i];
-  }
-  return r1;
-};
-AtomicFormula.prototype = Object.create(Formula.prototype);
-AtomicFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
-  if (typeof origTerm == 'string' && this.string.indexOf(origTerm) == -1) {
-    return this;
-  }
-  var newitems = AtomicFormula.substituteInTerms(
-    this.terms,
-    origTerm,
-    newTerm,
-    shallow
-  );
-  if (!this.terms.equals(newitems)) {
-    return new AtomicFormula(this.predicate, newitems);
-  } else return this;
-};
-AtomicFormula.substituteInTerms = function (terms, origTerm, newTerm, shallow) {
-  var newitems = [];
-  for (var i = 0; i < terms.length; i++) {
-    var term = terms[i];
-    if (term == origTerm) newitems.push(newTerm);
-    else if (term.isArray && !shallow) {
-      newitems.push(AtomicFormula.substituteInTerms(term, origTerm, newTerm));
-    } else newitems.push(term);
-  }
-  return newitems;
-};
-AtomicFormula.substituteInTerm = function (term, origTerm, newTerm) {
-  if (term == origTerm) return newTerm;
-  if (term.isArray)
-    return AtomicFormula.substituteInTerms(term, origTerm, newTerm);
-  return term;
-};
-function QuantifiedFormula(quantifier, variable, matrix, overWorlds) {
-  this.quantifier = quantifier;
-  this.matrix = matrix;
-  this.overWorlds = overWorlds;
-  this.variable = variable;
-  if (overWorlds) {
-    this.type = quantifier == '∀' ? 'modalGamma' : 'modalDelta';
-  } else {
-    this.type = quantifier == '∀' ? 'gamma' : 'delta';
-  }
-  this.string = quantifier + variable + matrix;
-}
-QuantifiedFormula.prototype = Object.create(Formula.prototype);
-QuantifiedFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
-  if (this.variable == origTerm) return this;
-  var nmat = this.matrix.substitute(origTerm, newTerm, shallow);
-  if (nmat == this.matrix) return this;
-  return new QuantifiedFormula(
-    this.quantifier,
-    this.variable,
-    nmat,
-    this.overWorlds
-  );
-};
-function BinaryFormula(operator, sub1, sub2) {
-  this.operator = operator;
-  this.sub1 = sub1;
-  this.sub2 = sub2;
-  this.type = operator == '∧' ? 'alpha' : 'beta';
-  this.string = '(' + sub1 + operator + sub2 + ')';
-}
-BinaryFormula.prototype = Object.create(Formula.prototype);
-BinaryFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
-  var ns11 = this.sub1.substitute(origTerm, newTerm, shallow);
-  var ns12 = this.sub2.substitute(origTerm, newTerm, shallow);
-  if (this.sub1 == ns11 && this.sub2 == ns12) return this;
-  return new BinaryFormula(this.operator, ns11, ns12);
-};
+
 function ModalFormula(operator, sub) {
   this.operator = operator;
-  this.sub = sub;
   this.type = operator == '□' ? 'modalGamma' : 'modalDelta';
+  this.sub = sub;
   this.string = operator + sub;
 }
 ModalFormula.prototype = Object.create(Formula.prototype);
 ModalFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
-  var ns1 = this.sub.substitute(origTerm, newTerm, shallow);
-  if (this.sub == ns1) return this;
-  return new ModalFormula(this.operator, ns1);
+  var n1 = this.sub.substitute(origTerm, newTerm, shallow);
+  if (this.sub == n1) return this;
+  return new ModalFormula(this.operator, n1);
 };
+
 function NegatedFormula(sub) {
   this.operator = '¬';
   this.sub = sub;
   this.type = NegatedFormula.computeType(sub);
   this.string = '¬' + sub;
 }
+NegatedFormula.prototype = Object.create(Formula.prototype);
+NegatedFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
+  var n1 = this.sub.substitute(origTerm, newTerm, shallow);
+  if (this.sub == n1) return this;
+  return new NegatedFormula(n1);
+};
 NegatedFormula.computeType = function (sub) {
   if (sub.operator == '¬') return 'doublenegation';
   switch (sub.type) {
     case 'literal': {
       return 'literal';
+    }
+    case 'modalDelta': {
+      return 'modalGamma';
     }
     case 'alpha': {
       return 'beta';
@@ -450,14 +375,92 @@ NegatedFormula.computeType = function (sub) {
     case 'modalGamma': {
       return 'modalBeta';
     }
-    case 'modalDelta': {
-      return 'modalGamma';
-    }
   }
 };
-NegatedFormula.prototype = Object.create(Formula.prototype);
-NegatedFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
-  var ns1 = this.sub.substitute(origTerm, newTerm, shallow);
-  if (this.sub == ns1) return this;
-  return new NegatedFormula(ns1);
+
+function BinaryFormula(operator, sub1, sub2) {
+  this.operator = operator;
+  this.sub1 = sub1;
+  this.sub2 = sub2;
+  this.type = operator == '∧' ? 'alpha' : 'beta';
+  this.string = '(' + sub1 + operator + sub2 + ')';
+}
+BinaryFormula.prototype = Object.create(Formula.prototype);
+BinaryFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
+  var num1 = this.sub1.substitute(origTerm, newTerm, shallow);
+  var num2 = this.sub2.substitute(origTerm, newTerm, shallow);
+  if (this.sub1 == num1 && this.sub2 == num2) {
+    return this;
+  }
+  return new BinaryFormula(this.operator, num1, num2);
+};
+
+function QuantifiedFormula(quantifier, variable, matrix, newVal) {
+  this.quantifier = quantifier;
+  this.matrix = matrix;
+  this.newVal = newVal;
+  this.variable = variable;
+  if (newVal) {
+    this.type = quantifier == '∀' ? 'modalGamma' : 'modalDelta';
+  } else {
+    this.type = quantifier == '∀' ? 'gamma' : 'delta';
+  }
+  this.string = quantifier + variable + matrix;
+}
+QuantifiedFormula.prototype = Object.create(Formula.prototype);
+QuantifiedFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
+  if (this.variable == origTerm) return this;
+  var n = this.matrix.substitute(origTerm, newTerm, shallow);
+  if (n == this.matrix) return this;
+  return new QuantifiedFormula(this.quantifier, this.variable, n, this.newVal);
+};
+
+function AtomicFormula(predicate, terms) {
+  this.type = 'literal';
+  this.terms = terms;
+  this.predicate = predicate;
+  this.string = predicate + AtomicFormula.items2string(terms);
+}
+AtomicFormula.prototype = Object.create(Formula.prototype);
+AtomicFormula.items2string = function (list) {
+  var r = '';
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].isArray) {
+      var newList = list[i].copy();
+      var x = newList.shift();
+      r += x + '(' + AtomicFormula.items2string(newList) + ')';
+    } else r += list[i];
+  }
+  return r;
+};
+AtomicFormula.prototype.substitute = function (origTerm, newTerm, shallow) {
+  if (typeof origTerm == 'string' && this.string.indexOf(origTerm) == -1) {
+    return this;
+  }
+  var newIt = AtomicFormula.substituteInTerms(
+    this.terms,
+    origTerm,
+    newTerm,
+    shallow
+  );
+  if (!this.terms.equals(newIt)) {
+    return new AtomicFormula(this.predicate, newIt);
+  } else return this;
+};
+AtomicFormula.substituteInTerm = function (term, origTerm, newTerm) {
+  if (term == origTerm) return newTerm;
+  if (term.isArray)
+    return AtomicFormula.substituteInTerms(term, origTerm, newTerm);
+  return term;
+};
+AtomicFormula.substituteInTerms = function (terms, origTerm, newTerm, shallow) {
+  var newIt = [];
+  for (var i = 0; i < terms.length; i++) {
+    var term = terms[i];
+    if (term == origTerm) newIt.push(newTerm);
+    else if (term.isArray && !shallow) {
+      newIt.push(AtomicFormula.substituteInTerms(term, origTerm, newTerm));
+    } else newIt.push(term);
+  }
+  return newIt;
 };
